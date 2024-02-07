fbefda69677d772e7255e1b7810ef6d3b7683776
Fernando Barrios - jfergt <fer.b.1990@gmail.com>
2024-02-07 14:15:48 +0000

    feat: update tests

--- sum.test.js
+++ sum.test.js
@@ -4,4 +4,9 @@ describe("probar el componente de suma", () => {
   it("que retorne valores correctos con numeros", () => {
     expect(sum(1, 2)).toBe(3);
   });
+
+  it("que retorne un error si se mandan valores no numericos", () => {
+    expect(() => sum("1", "2")).toThrow("Los argumentos deben ser numeros");
+    expect(() => sum("1", "2")).toThrow(TypeError);
+  });
 });
